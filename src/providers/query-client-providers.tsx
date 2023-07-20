4ac6e676201e254adcde27e352974a7291ef8a5f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-20 16:19:04 +0200

    modified the query provider

--- src/providers/query-client-providers.tsx
+++ src/providers/query-client-providers.tsx
@@ -1,23 +1,11 @@
 "use client"
 
-import React,{useState, useEffect} from "react";
+import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-
-
 const QueryClientProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-
-    useEffect(() => {
-      setIsMounted(true)
-    }, []);
-
-  return isMounted ? (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  ) : (
-    <>{children}</>
-  );
+  const queryClient = new QueryClient();
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 };
 
 export default QueryClientProviders;

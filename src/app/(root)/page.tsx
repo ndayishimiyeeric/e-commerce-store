a5d91f4fe8b9fe2fef0506146fb5d5d56e08e924
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-20 02:44:58 +0200

    refactor and add root folder

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,20 @@
-import {Button} from "@/components/ui/Button";
-import {UserButton} from "@clerk/nextjs";
+"use client"
+
+import React, {useEffect} from "react";
+import {useStoreModal} from "@/hooks/use-store-modal";
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOPen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+      if(!isOPen) {
+          onOpen();
+      }
+  }, [isOPen, onOpen])
   return (
       <div className="p-4">
-          <UserButton afterSignOutUrl="/" />
+          Root Page
       </div>
   )
 }

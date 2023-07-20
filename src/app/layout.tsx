d9d549257f60a3cfc94098817a78ba97d227b141
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-20 02:44:36 +0200

    add providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import {Inter} from 'next/font/google'
 import {ClerkProvider} from "@clerk/nextjs";
+import {ModalProvider} from "@/providers/modal-provider";
+import {ToastProvider} from "@/providers/toast-provider";
+import QueryClientProviders from "@/providers/query-client-providers";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -17,7 +20,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <QueryClientProviders>
+          <ToastProvider/>
+          <ModalProvider/>
+          <body className={inter.className}>{children}</body>
+        </QueryClientProviders>
       </html>
     </ClerkProvider>
   )

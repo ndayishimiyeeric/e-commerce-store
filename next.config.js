58574884619c3d7422cd9eeb09d524845e100cb9
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-21 07:33:50 +0200

    Add cloudinary as image upload service

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
   },
   experimental: {
     appDir: true

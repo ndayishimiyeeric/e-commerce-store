2354be0a393c5aada55026fd5d26fa088773b38f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-21 17:07:19 +0200

    Add colors route to main nav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -37,6 +37,11 @@ const MainNav = (
             active: pathname === `/${params.storeId}/sizes`,
         },
         {
+            href: `/${params.storeId}/colors`,
+            label: "Colors",
+            active: pathname === `/${params.storeId}/colors`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathname === `/${params.storeId}/settings`,

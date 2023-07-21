e9fc2013d28fcda503e1dd60f89790f3b1ae5ad6
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-21 16:58:16 +0200

    render sizes page, update page and creation page

---
+++ src/app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,32 @@
+import {format} from "date-fns";
+import {db} from "@/lib/db";
+import SizeClient from "@/components/size/client";
+import {SizeColumn} from "@/types/size-column";
+
+const SizesPage = async ({params}:{params: {storeId: string}}) => {
+    const sizes = await db.size.findMany({
+        where: {
+            storeId: params.storeId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    })
+
+    const formattedSizes = sizes.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: format(item.createdAt, "MMMM do, yyyy"),
+    })) satisfies SizeColumn[]
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-x-4 p-8 py-6">
+              <SizeClient sizes={formattedSizes} />
+            </div>
+        </div>
+    )
+};
+
+export default SizesPage;

e0f9b03eb81627efc77a4c094c72aa26f4eebd5b
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-21 10:39:55 +0200

    Fix api alert title

--- src/components/ui/ApiAlert.tsx
+++ src/components/ui/ApiAlert.tsx
@@ -26,11 +26,13 @@ const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
 const ApiAlert: React.FC<ApiAlertProps> = ({title, description, variant = "public"}) => {
     return (
         <Alert>
-            <Server className="h-4 w-4 mr-2" />
-            <AlertTitle className=" ml-5 flex items-center gap-x-2">
-                {title}
-                <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
-            </AlertTitle>
+            <div className="flex items-center space-x-2">
+                <Server className="h-4 w-4" />
+                <AlertTitle className="flex items-center gap-x-2">
+                    <span className="text-sm font-semibold">{title}</span>
+                    <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+                </AlertTitle>
+            </div>
             <AlertDescription className="mt-4 flex items-center justify-between">
                 <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">{description}</code>
                 <Button variant="outline" size="sm" onClick={() => onCopy(description)}>

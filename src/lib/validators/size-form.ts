11957c8220028c506307aa2f0eca07f3e1b26f3a
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-21 16:55:11 +0200

    Add size form validator

---
+++ src/lib/validators/size-form.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const SizeFormSchema = z.object({
+    name: z.string().min(1),
+    value: z.string().min(1),
+})
+
+export type SizeFormSchemaType = z.infer<typeof SizeFormSchema>

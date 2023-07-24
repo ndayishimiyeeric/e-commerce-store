c13d6ff8437a02a8bd58c09f5cd202bbc4e8664f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-24 07:41:56 +0200

    Add product form schema and type

---
+++ src/lib/validators/product-form.ts
@@ -0,0 +1,16 @@
+import {z} from "zod";
+
+export const ProductFormSchema = z.object({
+    name: z.string().min(1),
+    images: z.object({url: z.string()}).array(),
+    price: z.coerce.number().min(1),
+    quantity: z.coerce.number().min(1),
+    categoryId: z.string().min(1),
+    sizeId: z.string().min(1),
+    colorId: z.string().min(1),
+    isFeatured: z.boolean().default(false).optional(),
+    isArchived: z.boolean().default(false).optional(),
+})
+
+export type ProductFormSchemaType = z.infer<typeof ProductFormSchema>
+

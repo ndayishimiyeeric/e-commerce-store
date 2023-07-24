c023e7cb435ce19b28cc20b2e5ce00a5aae9a4ec
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-24 07:41:14 +0200

    Add product column type

---
+++ src/types/product-column.d.ts
@@ -0,0 +1,12 @@
+export type ProductColumn = {
+    id: string;
+    name: string;
+    quantity: number;
+    category: string;
+    isFeatured: boolean;
+    isArchived: boolean;
+    price: string;
+    size: string;
+    color: string;
+    createdAt: string;
+}
